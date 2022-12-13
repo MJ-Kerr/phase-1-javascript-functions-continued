12a8a193ca36ae2f102a6149859a49c57e1dd7b7
Miles Kerr <m.kerr.professional@gmail.com>
2022-12-12 20:14:48 -0600

    commit tree 77ffb3e4bdc6282b548fff8c091e5ce3dc90c5f3

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun() {
+    return 'roller-skate' === 'This Saturday, I want to roller-skate'
+}
+
+function mondayWork(){
+
+}
+function wrapAdjective(){
+
+}
